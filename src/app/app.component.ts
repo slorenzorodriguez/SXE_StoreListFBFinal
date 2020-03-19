1856ac911d5d5cdd801c49a151adffe9e0a27267
slorenzorodriguez <slorenzorodriguez@danielcastelao.orh>
2020-03-19 16:32:27 +0100

    Configuración Database RealTime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ServicioDeAutentService} from './servicio-de-autent.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'StoreList';
+  constructor( public authComponent: ServicioDeAutentService){
+  }
 }
+
